554c89d73f2a39aa45d4355a600404c56e6c9f4a
benhur carlos lp2 <benhur.10@hotmail.com>
2015-03-24 17:42:15 -0300

    Trabalho 1 com ArrayList

---
+++ exercicos/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int r = 0;
+            ArrayList ns = new ArrayList();
+
+            while (n != 0)
+            {
+                n = int.Parse(Console.ReadLine());
+
+                r = r + n;
+
+                if (n > 0)
+                {
+                    ns.Add(n);
+                }
+
+            }
+
+            r = r / ns.Count;
+
+            Console.WriteLine(r);
+            Console.ReadKey();
+
+        }
+    }
+}
+
