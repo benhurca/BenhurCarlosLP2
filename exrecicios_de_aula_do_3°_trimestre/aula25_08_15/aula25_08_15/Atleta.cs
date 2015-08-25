328a8eecabf8d3acd54a14fa598c72b8a519497b
benhurca <benhur.10@hotmail.com>
2015-08-25 17:28:28 -0300

    adiciona aula do dia 25/08/2015

---
+++ exrecicios_de_aula_do_3°_trimestre/aula25_08_15/aula25_08_15/Atleta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula25_08_15
+{
+    class Atleta
+    {
+        public string Nome { get; set; }
+        public string Nomes { get; set; }
+        public int TempoCs { get; set; }
+         public string TempoFormat
+        {
+            get
+            {
+               return String.Format ("{0}:{1}:{2}", TempoCs/6000,(TempoCs%6000)/100,(TempoCs%6000)%100);
+               
+            }
+        }
+
+    }
+}
